4f6c0f630cc75d1d7e1958fcf91eac85b3d57740
Manish Karki <manishkarki247@gmail.com>
2020-03-08 13:25:03 +0545

    add class search functionality

--- components/ClassCard.vue
+++ components/ClassCard.vue
@@ -1,24 +1,21 @@
 <template>
-  <div class="available-classes">
-    <div
-      v-for="availableClass in classes"
-      :key="availableClass.id"
-      class="class-card"
-      :style="`background-image:url(/Images/${availableClass.classImage})`"
-    >
-      <div class="class-card-overlay"></div>
-      <div class="class-card-info-wrapper">
-        <div class="class-card-info">
-          <div class="class-teacher-info-wrapper">
-            <img :src="`/Images/${availableClass.teacher.avatar}`" alt="User Avatar" />
-            <div class="class-teacher-info">
-              <h4>{{availableClass.teacher.name}}</h4>
-              <p>{{availableClass.className}}</p>
-            </div>
+  <div
+    class="class-card"
+    v-if="courseclass"
+    :style="`background-image:url(/Images/${courseclass.classImage})`"
+  >
+    <div class="class-card-overlay"></div>
+    <div class="class-card-info-wrapper">
+      <div class="class-card-info">
+        <div class="class-teacher-info-wrapper">
+          <img :src="`/Images/${courseclass.teacher.avatar}`" alt="User Avatar" />
+          <div class="class-teacher-info">
+            <h4>{{courseclass.teacher.name}}</h4>
+            <p>{{courseclass.className}}</p>
           </div>
-          <div class="class-description">
-            <p>{{availableClass.classDescription}}</p>
-          </div>
+        </div>
+        <div class="class-description">
+          <p>{{courseclass.classDescription}}</p>
         </div>
       </div>
     </div>
@@ -27,33 +24,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      classes: [
-        {
-          id: 'class001',
-          className: 'Web Development',
-          classDescription:
-            'Learn about web development with the best practices and hands on real life projects.',
-          classImage: 'laptop.jpg',
-          teacher: {
-            name: 'Arthur Pendragon',
-            avatar: 'avatar.png'
-          }
-        },
-        {
-          id: 'class002',
-          className: 'Mobile App Development',
-          classDescription:
-            'Get hands on experience in mobile development with Flutter for both Android and IOS.',
-          classImage: 'appdevelopment.jpg',
-          teacher: {
-            name: 'Ishtar',
-            avatar: 'femaleavatar.png'
-          }
-        }
-      ]
-    }
-  }
+  props: ['courseclass']
 }
 </script>
--- pages/app/dashboard/index.vue
+++ pages/app/dashboard/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="dashboard">
-    <form class="search-classes">
-      <input type="text" name="search" id="search" />
-      <input type="submit" value="Search" />
-    </form>
+    <div class="search-classes">
+      <input type="text" name="search" id="search" v-model="searchText" onchange="searchClass" />
+    </div>
+    <div class="available-classes">
+      <div v-for="availableClass in searchResults" :key="availableClass.id">
+        <ClassCard :courseclass="availableClass" />
+      </div>
+    </div>
     <ClassCard />
   </div>
 </template>
@@ -14,6 +18,44 @@ import ClassCard from '~/components/ClassCard'
 export default {
   components: {
     ClassCard
+  },
+  data() {
+    return {
+      searchText: '',
+      classes: [
+        {
+          id: 'class001',
+          className: 'Web Development',
+          classDescription:
+            'Learn about web development with the best practices and hands on real life projects.',
+          classImage: 'laptop.jpg',
+          teacher: {
+            name: 'Arthur Pendragon',
+            avatar: 'avatar.png'
+          }
+        },
+        {
+          id: 'class002',
+          className: 'Mobile App Development',
+          classDescription:
+            'Get hands on experience in mobile development with Flutter for both Android and IOS.',
+          classImage: 'appdevelopment.jpg',
+          teacher: {
+            name: 'Ishtar',
+            avatar: 'femaleavatar.png'
+          }
+        }
+      ]
+    }
+  },
+  computed: {
+    searchResults() {
+      return this.classes.filter(courseClass =>
+        courseClass.className
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase())
+      )
+    }
   }
 }
 </script>
